c22a496c2fb158f970c62f2202d953a3f1f1e33e
bojaum <36231883+bojaum@users.noreply.github.com>
2021-07-22 15:49:42 -0300

    New argument (stand)
    Include a option to choose between raw and std betas

--- R/sem_plotter.R
+++ R/sem_plotter.R
@@ -83,15 +83,18 @@ sem_plotter<-function(sem, stand=TRUE, layout=NULL, option="full", vertex.label=
   st.betas<-betas[ncorel,c(2,1,s,7)]
   p1<-st.betas[,4]<0.05
   st.betas<-st.betas[,1:3]
+  st.betas[,3]<-as.numeric(st.betas[,3])
   colnames(st.betas)[3]<-"Std.Estimate"
 
   st.cor<-betas[corel,c(2,1,8,7)]
   p2<-st.cor[,4]<0.05
   st.cor<-st.cor[,1:3]
+  st.cor[,3]<-as.numeric(st.cor[,3])
   st.cor[,1]<-gsub("~~", "", st.cor[,1])
   st.cor[,2]<-gsub("~~", "", st.cor[,2])
 
   st.dat<-rbind(st.betas, st.cor)
+
 
   net0<-graph_from_data_frame(st.dat, directed = TRUE)
   net1<-delete_edges(net0, corel)
